f8eb21b72bd2a8baad890f29e4cfdb9fef44ed09
MajesticThrust <majesticthrust@gmail.com>
2019-12-15 03:04:20 +0300

    assigned licence table controls

--- src/app/pages/app-utilization/assigned-licences/assigned-licences.component.ts
+++ src/app/pages/app-utilization/assigned-licences/assigned-licences.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-assigned-licences',
-  templateUrl: './assigned-licences.component.html',
-  styleUrls: ['./assigned-licences.component.scss']
+  selector: "app-assigned-licences",
+  templateUrl: "./assigned-licences.component.html",
+  styleUrls: ["./assigned-licences.component.scss"]
 })
 export class AssignedLicencesComponent implements OnInit {
+  public checkboxes = {
+    underutilizedLicences: false,
+    abandonedLicences: false
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public checkUnderutilizedLicences() {
+    this.checkboxes = {
+      underutilizedLicences: !this.checkboxes.underutilizedLicences,
+      abandonedLicences: false
+    };
   }
 
+  public checkAbandonedLicences() {
+    this.checkboxes = {
+      underutilizedLicences: false,
+      abandonedLicences: !this.checkboxes.abandonedLicences
+    };
+  }
 }
